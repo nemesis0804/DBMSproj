531bd96398e656b1ff5f7458a8102c26d872ff50
shreya-2604 <shreya10menon@gmail.com>
2024-11-17 16:13:33 +0530

    Fetching all campuses from backend

--- Components/Campuses/Campuses.jsx
+++ Components/Campuses/Campuses.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
 import './Campuses.css'
-import { campusList } from './CampusList'
 import CampusCard from './CampusCard'
 import Navbar from '../Navbar/Navbar'
-
+import axios from 'axios'
 
 const Campuses = () => {
+
+  const[campusList, setCampusList] = useState([]);
+  const[loading, setLoading] = useState(true);
+  const[error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:5454/campuses')
+    .then((response) => {
+      setCampusList(response.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setError("FAILED TO LOAD ALL CAMPUSES");
+      setLoading(false);
+    })
+  }, []);
+
   return (
     <div>
         <Navbar/>
@@ -17,15 +34,20 @@ const Campuses = () => {
             <h1>CAMPUSES</h1>
         </div>
     
-    
+      {loading? (
+        <p>LOADING...</p>
+      ) : error? (
+        <p>{error}</p>
+      ) : (
         <div className='background color- darkgrey px-10 flex flex-wrap items-center justify-around gap-1'>{
-           campusList.map((item) => <CampusCard title={item.title} image={item.image} add={item.add}/>) 
+           campusList.map((item) => <CampusCard title={item.name} image = {item.image} add={${item.address.street}, ${item.address.locality}, ${item.address.city}, ${item.address.zipcode}}/>) 
         }  
         </div>
+      )}
     </div>
     </div>
     
   )
 }
 
-export default Campuses
+export default Campuses;
